e3bfc9cf48cf1738770d8ee2d437d086cc09e9bd
Jervx <louellagracechua@gmail.com>
2022-04-03 20:07:59 +0800

    redo model & mongodb link

--- server/models/users.js
+++ server/models/users.js
@@ -1,23 +1,13 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt-nodejs')
-const userSchema=mongoose.Schema({
-  emailaddress:{type:String, required:true},
-  password:{type:String, required:true},
-  customer_address:{type:String, required:true},
-  customer_name:{type:String,required:true},
-  contact_no:{type:Number, required:true},
-  product_image:{type:String,required:true},
-  cartItems:{ type:[{
-    product_name: {type: mongoose.Schema.Types.ObjectID, ref: 'Product'},
-    product_qty: {type: Number, default: 1},
-    product_price: {type: Number, default: 0},
-    totalprice:{type:Number, default:0}
-}],default:[]}
-})
-userSchema.methods.generateHash = function (password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-  };
-userSchema.methods.validPassword = password => {
-    return bcrypt.compareSync(password, this.local.password);
-  };
-module.exports=mongoose.model('Login',userSchema)
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const userSchema = mongoose.Schema({
+  email_address: { type: String, required: true },
+  customer_address: { type: String, required: true },
+  customer_name: { type: String, required: true },
+  contact_no: { type: Number, required: true },
+  password: { type: String, required: true },
+  cartItems: {type: [{}], default: []},
+  orders : {type : [], default : []}
+});
+
+module.exports = mongoose.model("users", userSchema);
