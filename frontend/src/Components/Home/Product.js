284d5ecb581a07b5f8f1f9bed4a1ebf8201a72e7
unknown <dctrrohan@gmail.com>
2022-04-07 00:12:07 +0800

    layout heartss

--- frontend/src/Components/Home/Product.js
+++ frontend/src/Components/Home/Product.js
@@ -4,10 +4,10 @@ import { FaHeart } from "react-icons/fa"
 const Product = ({ product, onAddCart, onAddFav, liked, onRemoveFav, clickableAgain }) => {
   return (
     <>
-      <div className="group relative flex px-20 ss:flex-wrap md:px-2 xl:px-8 flex-col justify-evenly  border-[1px] items-center border-pink-70  border-[1px place-content-evenly h-auto md:mx-2 lg:mx-4 ">
-        <div className="w-[240px] bg-blue-500 relative ">
-          <div className=" bg-[#212121] absolute md:ml-0 ml-5 border-[#F29A4B] border-[1px] -translate-x-10 -left-3 z-10  -bottom-3 md:bottom-9 rounded-full ">
-      
+      <div className="group relative flex px-20 ss:flex-wrap md:px-2 xl:px-8 flex-col justify-evenly  border-[1px items-center border-pink-70 place-content-evenly h-auto md:mx-2 lg:mx-4 ">
+        <div className="w-[240px] bg-blu-500 relative ">
+          <div className=" bg-[#212121] absolute md:m76 xl-0 ml-5 border-[#F29A4B] border-[1px] -translate-x-16  top-5 z-30 rounded-full ">
+            <img src={product.image} className="h-20 w-20"></img>
           </div>
           <div className="bg-[#1e1e21]/90 bg-gren-500 border-[1px translate-x-5 duration-150 hover:bg-transparent col-span-3 h-[60px] h-max-[100px]
                         w-[200px] max-w-[200px]  pl-10 after:content-[''] after:w-[90%] after:h-[2px] after:bg-[#F29A4B] after:left-0 after:bottom-0 after:top-[110%] after:absolute
@@ -32,12 +32,18 @@ const Product = ({ product, onAddCart, onAddFav, liked, onRemoveFav, clickableAg
               </button>
             </div>
           </div>
-          <FaHeart onClick={()=> {
-              if(!clickableAgain) return
-              if(liked) onRemoveFav(product._id)
-              else onAddFav(product._id)
-          }} className={`w-10 h-10 ${liked ? 'text-rose-500' : 'text-neutral-600'}`}/>
-          <p>{product.total_likes}</p>
+          <div className="border-[1px mt-4 text-[.8rem] md:text-sm flex items-center justify-evenly">
+            <p className="grow">Likes</p>
+            <div className="flex-none flex px-4 gap-3">
+              <p>{product.total_likes}</p>
+              <FaHeart onClick={() => {
+                if (!clickableAgain) return
+                if (liked) onRemoveFav(product._id)
+                else onAddFav(product._id)
+              }} className={` w-8 h-6 ${liked ? 'text-rose-500' : 'text-neutral-600'}`} />
+            
+            </div>
+          </div>
         </div>
       </div>
 
