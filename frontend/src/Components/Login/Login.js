9cd151f4748a32ac309cbc1716d5bb4ef571d941
unknown <dctrrohan@gmail.com>
2022-04-07 10:47:54 +0800

    fixing login layout

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -13,155 +13,129 @@ export const Login = () => {
   let history = useHistory();
 
   const signIn = async () => {
-      setLoading(true)
+    setLoading(true)
     try {
       setError('')
       const response = await API.post("/login", { email_address, password });
       saveUser(response);
-      if(remember)
+      if (remember)
         rememberMe(email_address, password)
       history.push("/home");
     } catch (e) {
       console.log(e);
-      if(e.response.data) setError(e.response.data.description)
+      if (e.response.data) setError(e.response.data.description)
       else setError("Sorry but we can't reach the server")
       setLoading(false)
     }
   };
 
-  useEffect(()=>{
-      const remembered = getRemembered()
-      if(remembered){
-         setEmail(remembered.email_address)
-         setPassword(remembered.password)
-      }
+  useEffect(() => {
+    const remembered = getRemembered()
+    if (remembered) {
+      setEmail(remembered.email_address)
+      setPassword(remembered.password)
+    }
   }, [])
+  const [openTab, setOpenTab] = React.useState(1)
 
   return (
     <>
-      <div className="flex bg-[#24262B]  w-full justify-center md:h-screen lg:h-screen xl:h-screen items-center">
-        <div
-          className="flex bg-[#141517] relative justify-center items-center h-screen w-full border-0 md:justify-center
-         md:items-center md:max-h-[55rem]  xl:max-w-[90%] md:border-white md:border-y-[1px] md:border-opacity-25 lg:max-w-[100%] 
-         2xl:border-[#F29A4B] 2xl:rounded-[47px]"
-        >
-          {/* <img alt=''  src={require('../../img/rectangle.png')} className="absolute invisible md:max-w-[700px] md:visible md:w-[80%] md:h-auto"></img> */}
-          {/*just adding comment 2xl:border-white border-[.2rem]         */}
-          <div className="2xl:absolute 2xl:left-[10vh] 2xl:h-auto 2xl:flow-root 2xl:">
-            <img
-              alt=""
-              src={require("../../img/text.png")}
-              className="invisible 2xl:visible 2xl:min-h-[300px] 2xl:max-h-full"
-            ></img>
-          </div>
-          <img
-            alt=""
-            src={require("../../img/login_bg.png")}
-            className=" invisible md:max-w-[700px] md:visible md:w-[80%] md:h-auto 
-          xl:max-w-[900px] xl:min-h-[500px] xl:max-h-full 2xl: 2xl:max-w-[900px] 2xl:min-h-[500px] 2xl:max-h-full"
-          ></img>
-
-          <div
-            className="flex flex-col absolute w-full h-[80vh] border-[#F29A4B] justify-center md:border
-           md:border-[#F29A4B] md:bg-opacity-50 border-opacity-25 md:rounded-[47px] md:max-w-[60%] md:border-none"
-          >
-            <img
-              alt=""
-              src={require("../../img/d.png")}
-              className="max-w-[500px] w-full absolute self-center md:invisible blur-[15px]"
-            ></img>
-            <div className="flex flex-col w-full h-auto px-12 bg-[#0f0f11] bg-opacity-[80%] border-white border-y-[1px] md:max-w-[80%] md:translate-x-[4.5rem] border-opacity-25 justify-center z-10 md:w-full md:h-auto md:rounded-[47px] md:py-0 md:px-7 xl:max-w-[65%] xl:translate-x-[25%] 2xl:max-w-[50%] 2xl:translate-x-[50%] 2xl:border-[#F29A4B] 2xl:px-9">
-              <img
-                alt=""
-                src={require("../../img/logo.png")}
-                className="h-auto w-[130px] md:max-w-[30%] self-center z-10 py-3 lg:w-[120px] lg:h-auto"
-              ></img>
-              <h1 className="w-fit self-center py-2 text-[#CBCBCB] text-[1rem] tracking-[0.25rem] font-medium font-['Nunito Sans'] z-10 lg:py-0">
-                SIGN IN TO DJOYOF
-              </h1>
-              <form className="flex flex-col">
-                <div className="mb-4 py-2 z-10">
-                  <label
-                    className="block mb-1 text-sm text-[#CBCBCB] font-['Nunito Sans'] tracking-[0.1rem] z-10"
-                    for="email"
-                  >
-                    Email
-                  </label>
+      <div className="bg-[#323439] flex justify-center items-center w-screen h-screen overflow-auto lg:px-10 xl:border-[1px xl:px-[7rem] 2xl:px-[6rem] 3xl:px-[7rem] md:py-18 lg:py-20">
+        <div className="flex bg-[#141517 bg-[#141517] relative justify-center items-center w-full border-0 h-full md:border-y-[1px] xl:rounded-[2.9rem] md:border-opacity-25 2xl:border-[#F29A4B] 
+                     2xl:rounded-[47px]  bg-center bg-no-repeat xl:bg-[length:750px_auto] 2xl:bg-[length:1000px_auto] md:px-28 lg:px-[16rem] "
+          style={{ backgroundImage: `${openTab === 1 && "url('https://cdn.discordapp.com/attachments/755283323110293547/961084398848057374/bg.png')"}`, }}>
+          <img className="absolute lg:left-10 invisible lg:visible xl:left-16 2xl:left-20 lg:h-70 lg:w-auto "
+            src={require("../../img/text.png")} ></img>
+          <div className="flex flex-col w-full h-auto px-12  bg-[#0f0f11] bg-opacity-[80%] md:rounded-[1rem] xl:max-w-xl border-[#ed852bd5] border-y-[.5px]  ">
+            <img alt="" src={require("../../img/logo.png")} className="h-auto w-[130px] md:max-w-[30%] self-center z-10 py-3 lg:w-[120px] lg:h-auto"></img>
+            <h1 className="w-fit self-center py-2 text-[#CBCBCB] text-[1rem] tracking-[0.25rem] font-medium z-10 lg:py-0">
+              SIGN IN TO DJOYOF
+            </h1>
+            <form className="flex flex-col border-[1px font-nsans md:text-sm lg:text-[0.99rem] border-green-500">
+              <div className="mb-4 py-2 z-10">
+                <label
+                  className="block mb-1  text-[#CBCBCB]  tracking-[0.1rem] z-10"
+                  for="email"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  value={email_address}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  name="email"
+                  className="text-[#CBCBCB]  py-2 px-3 w-full h-[37px] bg-[#1A1B1E] border-[#1A1B1E] focus:outline-none border border-b-[#F29A4B] border-opacity-50"
+                />
+              </div>
+              <div className="mb-4 z-10">
+                <label
+                  className="block mb-1  text-[#CBCBCB] tracking-[0.1rem]"
+                  for="password"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="text-[#CBCBCB] py-2 px-3 w-full h-[37px] bg-[#1A1B1E] border-[#1A1B1E] focus:outline-none border border-b-[#F29A4B] border-opacity-50 "
+                />
+              </div>
+              <div className="mt-6 flex items-center justify-between">
+                <div className="flex items-center z-10">
                   <input
-                    id="email"
-                    value={email_address}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="text"
-                    name="email"
-                    className="text-[#CBCBCB] font-['Nunito Sans'] py-2 px-3 w-full h-[37px] bg-[#1A1B1E] border-[#1A1B1E] focus:outline-none border border-b-[#F29A4B] border-opacity-50"
+                    id="remember_me"
+                    type="checkbox"
+                    value={remember}
+                    onChange={(e) => { setRemember(e.target.value) }}
+                    className="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50"
                   />
-                </div>
-                <div className="mb-4 z-10">
                   <label
-                    className="block mb-1  text-sm text-[#CBCBCB] tracking-[0.1rem]"
-                    for="password"
+                    for="remember_me"
+                    className="ml-2 block text-sm lg:text-[0.90rem] leading-5 text-[#a3a3a3]"
                   >
-                    Password
-                  </label>
-                  <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="text-[#CBCBCB] py-2 px-3 w-full h-[37px] bg-[#1A1B1E] border-[#1A1B1E] focus:outline-none border border-b-[#F29A4B] border-opacity-50 "
-                  />
-                </div>
-                <div className="mt-6 flex items-center justify-between">
-                  <div className="flex items-center z-10">
-                    <input
-                      id="remember_me"
-                      type="checkbox"
-                      value={remember}
-                      onChange={(e)=>{setRemember(e.target.value)}}
-                      className="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50"
-                    />
-                    <label
-                      for="remember_me"
-                      className="ml-2 block text-sm leading-5 text-[#a3a3a3]"
-                    >
-                      {" "}
-                      Remember me{" "}
-                    </label>
-                  </div>
-                  <a href="/" className="text-sm text-[#1CADFF] z-10">
                     {" "}
-                    Forgot your password?{" "}
-                  </a>
+                    Remember me{" "}
+                  </label>
                 </div>
-                <div className={`justify-center ${loading || error.length > 0 ? 'block' : 'hidden'}`}>
-                    <AiOutlineLoading className={`my-2 mx-auto w-5 h-5 text-orange-500 animate-spin ${loading ? 'block' : 'hidden'}`}/>
-                    <p className={`text-center text-xs text-gray-600  ${loading ? 'block' : 'hidden'}`}>Wait loading puking ina...</p>
-                    <p className="text-xs my-4 text-rose-400 text-center">{error}</p>
-                </div>
-                <div className="py-[45px] lg:py-[20px] flex items-center justify-center z-10">
-                  <button
-                    disabled={email_address.length === 0 || password.length === 0 || loading}
-                    onClick={(e) => {
-                        e.preventDefault()
-                        signIn()
-                    }}
-                    className={`${email_address.length === 0 && password.length === 0 && 'disabled'}  text-sm font-['Nunito Sans'] tracking-[0.1rem] w-30 px-4 py-1  bg-[#cc9361] self-center border border-transparent rounded-md font-semibold capitalize text-white hover:bg-[#F29A4B] focus:outline-none disabled:opacity-25 transition`}
-                  >
-                    Sign In
-                  </button>
-                </div>
-                <div className="text-center z-10 mt-1 pb-7">
-                  <a
-                    href="/"
-                    className="underline font-['Nunito Sans']  text-sm text-[#CBCBCB]"
-                  >
-                    Sign up for an account
-                  </a>
-                </div>
-              </form>
-            </div>
+                <a href="/" className="text-sm lg:text-[0.90rem]  text-[#1CADFF] z-10">
+                  {" "}
+                  Forgot your password?{" "}
+                </a>
+              </div>
+              <div className={`justify-center ${loading || error.length > 0 ? 'block' : 'hidden'}`}>
+                <AiOutlineLoading className={`my-2 mx-auto w-5 h-5 text-orange-500 animate-spin ${loading ? 'block' : 'hidden'}`} />
+                <p className={`text-center text-xs text-gray-600  ${loading ? 'block' : 'hidden'}`}>Wait loading puking ina...</p>
+                <p className="text-xs my-4 text-rose-400 text-center">{error}</p>
+              </div>
+              <div className="py-[45px] lg:py-[20px] flex items-center justify-center z-10">
+                <button
+                  disabled={email_address.length === 0 || password.length === 0 || loading}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    signIn()
+                  }}
+                  className={`${email_address.length === 0 && password.length === 0 && 'disabled'} 
+                     text-sm font-['Nunito Sans'] tracking-[0.1rem] w-30 px-4 py-1  bg-[#cc9361] self-center rounded-md font-semibold capitalize text-white hover:bg-[#F29A4B] focus:outline-none  transition`}
+                >
+                  Sign In
+                </button>
+              </div>
+              <div className="text-center z-10 mt-1 pb-7">
+                <a
+                  href="/"
+                  className=" underline font-['Nunito Sans']  text-sm lg:text-[0.90rem]  text-[#CBCBCB]"
+                >
+                  Sign up for an account
+                </a>
+              </div>
+            </form>
           </div>
+          <img className="absolute invisible lg:visible lg:right-16 lg:bottom-0 h-full lg:h-70 lg:w-auto"
+            src={require("../../img/foot.png")} ></img>
         </div>
       </div>
     </>
--- frontend/src/Components/Login/Signup.js
+++ frontend/src/Components/Login/Signup.js
@@ -12,7 +12,7 @@ export const Signup = () => {
 
     return (
         <>
-            <div className="bg-[#323439] flex justify-center items-center w-screen h-screen overflow-auto  lg:px-10 xl:px-20">
+            <div className="bg-[#323439] flex justify-center items-center w-screen h-screen overflow-auto lg:px-10 xl:px-[5rem] 2xl:px-[6rem] 3xl:px-[7rem]">
 
                 <div className="relative bg-[#141517] lg:border-[.2px] lg:border-[#D2A781] flex justify-center bg-center bg-no-repeat bg-[length:750px_900px]
                                  items-center w-full h-[80vh] lg:rounded-[1.7rem] lg:px-20 xl:rounded-[3.7rem] rounded-2xl  xl:px-32 md:px-2 py-10 2xl:px-[15rem] 3xl:px-[25rem]"
@@ -30,8 +30,9 @@ export const Signup = () => {
                                 <h2 className="text-[#F29A4B] font-nsans text-2xl xl:text-[3rem] italic font-extrabold lg:tracking-[.4rem] text-left w-[250px] lg:w-[340px] xl:w-[340px] border-[1px">DJOYOF</h2>
                                 <h1 className="font-nsans font-black text-[#CFCFCF] tracking-[.2em] xl:tracking-[1rem] lg:tracking-[1.5rem] text-5xl xl:text-[4rem] border-[1px ">BAKING</h1>
                             </div>
-                            <p className="text-justify font-pop text-sm ">
+                            <p className="text-justify font-pop text-sm tracking-wide">
                                 DJOYOFBAKING is founded by Ms. Dimple Cuevas.
+                                <br></br>
                                 <br></br>
                                 DJOYOFBAKING is a small sole proprietorship business which cater and promotes fresh baked home local and international breads.
                                 <br></br>
@@ -41,12 +42,12 @@ export const Signup = () => {
                         </div>
                         {/* right side */}
                         <div className="w-full h-full border-[1px xl:rounded-r-3xl  rounded-r-2xl  px-10 flex flex-col gap-5 justify-center py-6 items-center border-[1px">
-                            <h2 className="uppercase font-nsan border-[1px font-semibold " >Sign up to DJOYOF</h2>
+                            <h2 className="uppercase font-nsan border-[1px font-semibold md:text-[1.1rem] tracking-widest " >Sign up to DJOYOF</h2>
                             {/* input */}
-                            <div className="w-full border-[1px gap py-4 flex flex-col gap-4">
-                                <div className="border-[1px ">
+                            <div className="w-full border-[1px text-sm md:text-[.7rem] lg:text-[.8rem] gap py-4 flex flex-col gap-4">
+                                <div className="border-[1px  ">
                                     <div className="flex">
-                                        <label className=" block mb-1 font-nsans text-xs text-[#CBCBCB] tracking-[0.1rem]" for="password"> Email </label>
+                                        <label className=" block mb-1 font-nsans  text-[#CBCBCB] tracking-[0.1rem]" for="password"> Email </label>
                                         <HiOutlineMail className="w-7 h-5">
                                         </HiOutlineMail>
                                     </div>
@@ -54,7 +55,7 @@ export const Signup = () => {
                                 </div>
                                 <div className="border-[1px  ">
                                     <div className="flex">
-                                        <label className="block mb-1 font-nsans text-xs text-[#FFAA5D] tracking-[0.1rem]" for="password"> address </label>
+                                        <label className="block mb-1 font-nsans  text-[#FFAA5D] tracking-[0.1rem]" for="password"> address </label>
                                         <FaRegAddressBook className="w-7 h-5">
                                         </FaRegAddressBook>
                                     </div>
@@ -62,7 +63,7 @@ export const Signup = () => {
                                 </div>
                                 <div className="border-[1px  ">
                                     <div className="flex">
-                                        <label className="block mb-1 font-nsans text-xs text-[#FFAA5D] tracking-[0.1rem]" for="password"> contact no </label>
+                                        <label className="block mb-1 font-nsans  text-[#FFAA5D] tracking-[0.1rem]" for="password"> contact no </label>
                                         <MdContactPhone className="w-7 h-5">
                                         </MdContactPhone>
                                     </div>
@@ -70,7 +71,7 @@ export const Signup = () => {
                                 </div>
                                 <div className="border-[1px  ">
                                     <div className="flex">
-                                        <label className="block mb-1 font-nsans text-xs text-[#FFAA5D] tracking-[0.1rem]" for="password"> password </label>
+                                        <label className="block mb-1 font-nsans  text-[#FFAA5D] tracking-[0.1rem]" for="password"> password </label>
                                         <RiLockPasswordLine className="w-7 h-5">
                                         </RiLockPasswordLine>
                                     </div>
@@ -78,13 +79,15 @@ export const Signup = () => {
                                 </div>
                                 <div className="border-[1px  ">
                                     <div className="flex">
-                                        <label className="block mb-1 font-nsans text-xs text-[#FFAA5D] tracking-[0.1rem]" for="password"> confirm password </label>
+                                        <label className="block mb-1 font-nsans  text-[#FFAA5D] tracking-[0.1rem]" for="password"> confirm password </label>
                                         <GiConfirmed className="w-7 h-5">
                                         </GiConfirmed>
                                     </div>
                                     <input className="text-[#CBCBCB] w-full  h-[30px] bg-[#1a1b1ed4] border-[#1a1b1ed4] focus:outline-none border border-b-[#FFFFFF] border-opacity-50" id="" type="" name="" value='' />
                                 </div>
-                                <button className="px-3 py-1 lg:my-4  w-32 h-auto rounded-md text-[15px] text-[#302C2C] font-nsans font-bold lowercase bg-[#F29A4B] hover:bg-[#f48422] hover:text-[1.1rem] ">Sign In</button>
+                                <div className="flex justify-center">
+                                    <button className="px-3 py-1 lg:my-4  w-32 h-auto rounded-md text-[15px] text-[#302C2C] font-nsans font-bold lowercase bg-[#F29A4B] hover:bg-[#f48422] hover:text-[1.1rem] ">Sign In</button>
+                                </div>
                             </div>
                         </div>
                     </div>
